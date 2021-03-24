85c33a0132887512d9864bda5995c29a20503c06
tharusha-kudagala <tharushakudagala1997@gmail.com>
2021-03-24 17:03:12 +0530

    Finished

--- routes/sms.route.js
+++ routes/sms.route.js
@@ -17,6 +17,36 @@ const {
  * @param {String} complaintTitle complaint title
  */
  router.post("/accept/:userPhone/:userFirstName/:complaintID/:complaintTitle", SendAcceptSMSController);
+  
+ /**
+ * @description Send SMS : Notify reject the complaint
+ * @name post/sms/reject
+ * @param {String} userPhone complaint user phonenumber
+ * @param {String} userFirstName complaint user first name
+ * @param {String} complaintID complaint id
+ * @param {String} complaintTitle complaint title
+ */
+   router.post("/reject/:userPhone/:userFirstName/:complaintID/:complaintTitle", SendRejectSMSController);
+
+  
+ /**
+ * @description Send SMS : Any
+ * @name post/sms/send
+ * @param {String} userPhone complaint user phonenumber
+ * @param {String} smsBody body of the message
+ */
+  router.post("/send/:userPhone/:smsBody", SendAnySMSController);
+
+/**
+ * @description Send Email : Completion confirmation
+ * @name post/sms/complete/
+ * @param {String} userPhone
+ * @param {String} userFirstName
+ * @param {String} complaintID
+ * @param {String} complaintTitle
+ * @param {String} link confirmation link
+ */
+ router.post("/complete/:userPhone/:userFirstName/:complaintID/:complaintTitle/:link", SendCompleteSConfirmationController);
 
 
   module.exports = router;
